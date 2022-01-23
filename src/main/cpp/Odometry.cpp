c018be30a34230f54c43a0127ad4100fc248a481
Andrew <54419795+AndrewKim1S@users.noreply.github.com>
2022-01-22 19:17:23 -0800

    Add files via upload

---
+++ src/main/cpp/Odometry.cpp
@@ -0,0 +1,85 @@
+#include <Odometry.h>
+
+//Constructor
+Odometry::Odometry(){}
+
+
+//Updates the Odometry Robot location
+//https://www.first1684.com/uploads/2/0/1/6/20161347/chimiswerve_whitepaper__2_.pdf
+//https://www.chiefdelphi.com/t/calculating-odometry-of-a-swerve-drive/160043/6
+void
+Odometry::updateOdometry(double BR_A, double BR_V,
+            double BL_A, double BL_V,
+            double FR_A, double FR_V,
+            double FL_A, double FL_V,
+            double theta){
+
+    double B_FL = sin(FL_A * PI/180) * FL_V;
+    double A_RL = sin(BL_A * PI/180) * BL_V;
+    double A_RR = sin(BR_A * PI/180) * BR_V;
+    double D_FL = cos(FL_A * PI/180) * FL_V;
+    double C_FR = cos(FR_A * PI/180) * FR_V;
+    double B_FR = sin(FR_A * PI/180) * FR_V;
+    double D_RL = cos(BL_A * PI/180) * BL_V;
+    //Potential Typo
+    double C_RR = cos(BR_A * PI/180) * BR_V;
+
+    double A = (A_RR + A_RL) / 2;
+    double B = (B_FL + B_FR) / 2;
+    double C = (C_FR + C_RR) / 2;
+    double D = (D_FL + D_RL) / 2;
+
+    double ROT1 = (B-A)/m_L;
+    double ROT2 = (C-D)/m_W;
+    double ROT = (ROT1 + ROT2) / 2;
+
+    m_FWD = ((ROT * (m_L/2) + A) + (-ROT * (m_L/2) + B)) / 2;
+    m_STR = ((ROT * (m_W/2) + C) + (-ROT * (m_W/2) + D)) / 2;
+
+    m_FWD_new = m_FWD * cos(theta * PI/180) + m_STR * sin(theta * PI/180);
+    m_STR_new = m_STR * cos(theta * PI/180) - m_FWD * sin(theta * PI/180);
+    // frc::SmartDashboard::PutNumber("m_FWD", m_FWD_new);
+    // frc::SmartDashboard::PutNumber("m_STR", m_STR_new);
+    
+    //From Velocity to Position
+    m_prevtimeStep = m_timeStep;
+    m_timeStep += timeInterval;
+    //frc::SmartDashboard::PutNumber("Time", m_timeStep);
+    m_x = m_x + m_FWD_new * (m_timeStep - m_prevtimeStep) * 58.04;
+    m_y = m_y + m_STR_new * (m_timeStep - m_prevtimeStep) * 58.04;
+}
+
+
+//Return X position
+double
+Odometry::getX(){
+    return m_x;
+}
+
+
+//Return Y position
+double
+Odometry::getY(){
+    return m_y;
+}
+
+
+double
+Odometry::getYSpeed(){
+    return m_FWD_new;
+}
+
+
+double
+Odometry::getXSpeed(){
+    return m_STR_new;
+}
+
+
+//Reset Odometry
+void
+Odometry::Reset(){
+    m_x = 0;
+    m_y = 0;
+    m_timeStep = 0;
+}
---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,116 @@
+#include "Robot.h"
+#include <iostream>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  try{
+    navx = new AHRS(frc::SPI::Port::kMXP);
+  } catch(const std::exception& e){
+    std::cout << e.what() <<std::endl;
+  }
+}
+
+
+void Robot::RobotPeriodic() {}
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+
+void Robot::TeleopInit() {
+  m_time_step = 0;
+  navx->Reset();
+}
+
+
+void Robot::TeleopPeriodic() {
+  
+  m_time_step += 0.02;
+  if(m_time_step <= 1.20){
+    m_swerve.Initialize();
+  }
+  else if(m_time_step > 1.20 && m_time_step < 1.40){
+    m_swerve.ResetEncoders();
+  } 
+
+  else if(m_time_step > 1.40){
+
+    //Deadband 
+    double x1, y1, x2;
+    x1 = l_joy.GetRawAxis(0);
+    y1 = l_joy.GetRawAxis(1) * 0.6;
+    x2 = r_joy.GetRawAxis(0);
+    x1 = abs(x1) < 0.05 ? 0.0: x1;
+    y1 = abs(y1) < 0.05 ? 0.0: y1;
+    x2 = abs(x2) < 0.05 ? 0.0: x2;
+
+    m_swerve.Drive(-x1, -y1, -x2, navx->GetYaw(), true);
+
+    m_swerve.UpdateOdometry(navx->GetYaw());
+    
+    //Button (A)
+    if(xbox.GetRawButton(1)){
+      std::cout << "reset Odometry" << std::endl;
+      m_swerve.ResetOdometry();
+    }
+
+    if(xbox.GetRawButton(2)){
+      std::cout << "Set Swerve Controller PID" << std::endl;
+      m_swerve.SetDriveControllerPID();
+      //m_swerve.debug();
+    }
+
+    if(xbox.GetRawButton(3)){
+      std::cout << "Set Rot Controller PID" << std::endl;
+      m_swerve.SetDriveControllerROTPID();
+    }
+
+    frc::SmartDashboard::PutNumber("Y", m_swerve.GetYPosition());
+    frc::SmartDashboard::PutNumber("X", m_swerve.GetXPostion());
+  }
+}
+
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+
+void Robot::TestInit() {
+  m_time_step = 0;
+  navx->Reset();
+  m_swerve.ResetOdometry();
+  m_swerve.GenerateTrajectory_1();
+}
+
+
+void Robot::TestPeriodic() {
+  m_time_step += 0.02;
+  frc::SmartDashboard::PutNumber("Y", m_swerve.GetYPosition());
+  frc::SmartDashboard::PutNumber("X", m_swerve.GetXPostion());
+
+  if(m_time_step <= 1.20){
+    m_swerve.Initialize();
+  }
+  
+  else if(m_time_step > 1.20 && m_time_step < 1.40){
+    m_swerve.ResetEncoders();
+  } 
+
+  else if(m_time_step > 1.40){
+    m_swerve.UpdateOdometry(navx->GetYaw());
+    m_swerve.TrajectoryFollow(navx->GetYaw(), false);
+
+        
+  }
+}
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
