fba18eb702f46ae49c3f22474d260f31e8a1489c
Susan Clay <47490118+SusanC3@users.noreply.github.com>
2022-02-02 19:42:18 -0800

    Start shooter testing

--- src/test/cpp/main.cpp
+++ src/test/cpp/main.cpp
@@ -1,9 +1,26 @@
-#include <hal/HAL.h>
-
 #include "gtest/gtest.h"
+#include "Shooter.h"
+#include "Constants.h"
+#include "frc/DigitalInput.h"
+#include "frc/simulation/DIOSim.h"
+#include <ctre/Phoenix.h>
+#include <iostream>
+
+
+class TestShooter : public testing::Test {
+    protected:
+        Shooter shooter;
+        frc::sim::DIOSim m_turretLimitSwitch{shooter.getLimitSwitch()};
+};
+
+TEST_F(TestShooter, ZeroNotDoneTest) {
+    m_turretLimitSwitch.SetValue(false); //not tripped
+    shooter.Zero();
+    EXPECT_DOUBLE_EQ(0.25, shooter.getFlywheelMaster().GetMotorOutputPercent());
+    std::cout << "current: " << shooter.getFlywheelMaster().GetOutputCurrent() << "\n";
+}
 
 int main(int argc, char** argv) {
-  HAL_Initialize(500, 0);
   ::testing::InitGoogleTest(&argc, argv);
   int ret = RUN_ALL_TESTS();
   return ret;
