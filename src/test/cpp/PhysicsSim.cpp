dd452e012dfe374e82e83afdf44bb6e969929702
Susan Clay <47490118+SusanC3@users.noreply.github.com>
2022-02-04 14:48:52 -0800

    Add ctre simulation support

---
+++ src/test/cpp/PhysicsSim.cpp
@@ -0,0 +1,29 @@
+#include <simulation/PhysicsSim.h>
+
+PhysicsSim& PhysicsSim::GetInstance() {
+    static PhysicsSim sim;
+    return sim;
+}
+
+PhysicsSim::~PhysicsSim() {
+    _simProfiles.clear();
+}
+
+void PhysicsSim::AddTalonSRX(TalonSRX &talon, double const accelToFullTime, double const fullVel, bool const sensorPhase) {
+    TalonSRXSimProfile* simTalon = new TalonSRXSimProfile(talon, accelToFullTime, fullVel, sensorPhase);
+    this->_simProfiles.insert(this->_simProfiles.end(), simTalon);
+}
+
+void PhysicsSim::Run() {
+    for (auto simProfile : _simProfiles) {
+        simProfile->Run();
+    }
+}
+
+double PhysicsSim::random(double min, double max) {
+    return (max - min) / 2 * sin(fmod(rand(), 2 * 3.14159)) + (max + min) / 2;
+}
+
+double PhysicsSim::random(double max) {
+    return random(0, max);
+}
---
+++ src/test/cpp/SimProfile.cpp
@@ -0,0 +1,17 @@
+#include <simulation/PhysicsSim.h>
+
+double PhysicsSim::SimProfile::GetPeriod() {
+    // set the start time if not yet running
+    if (!_running) {
+        _lastTime = std::chrono::steady_clock::now();
+        _running = true;
+    }
+
+    // get time since last call
+    auto now = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> elapsed(now - _lastTime);
+    double const period = elapsed.count();
+    _lastTime = now;
+
+    return period;
+}
