b45134240312f8b77bdeaa6c84af38bbe0ec0eec
SusanC3 <susanclay333@gmail.com>
2022-02-17 18:49:19 -0800

    add frame periods class

---
+++ RobotCode/src/main/cpp/FramePeriods.cpp
@@ -0,0 +1,37 @@
+#include "FramePeriods.h"
+
+FramePeriods::FramePeriods() {
+
+}
+
+void FramePeriods::addTalon(WPI_TalonFX& talon) {
+   talons.push_back(&talon);
+  //  setFramePeriods(talon);
+}
+
+void FramePeriods::periodic() {
+    for (WPI_TalonFX* talon: talons) {
+        checkFramePeriods(talon);
+    }
+}
+
+void FramePeriods::setFramePeriods(WPI_TalonFX& talon) {
+    ErrorCollection err;
+    for (int i = 0; i < statusFrameAttempts; i++) {
+     //   err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_1_General, st));
+      //  err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_2_Feedback0, st));
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_3_Quadrature, lt));
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_4_AinTempVbat, lt));
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_8_PulseWidth, lt));
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_10_MotionMagic, lt));
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_12_Feedback1, lt));
+     //   err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_13_Base_PIDF0, st)); //idk if we actually use
+        err.NewError(talon.SetStatusFramePeriod(StatusFrameEnhanced::Status_14_Turn_PIDF1, lt));
+        if (err.GetFirstNonZeroError() == ErrorCode::OK) return;
+    }
+    
+}
+
+void FramePeriods::checkFramePeriods(WPI_TalonFX* talon) {
+    if ((*talon).HasResetOccurred()) setFramePeriods(*talon);   
+}
