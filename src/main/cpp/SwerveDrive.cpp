0f501074432262a4a746171ad0f53b832a43153c
Josh Hejna <josh.hejna@gmail.com>
2022-03-22 13:56:11 -0700

    Swerve odometry based on simple averages and encoder distances

--- src/main/cpp/SwerveDrive.cpp
+++ src/main/cpp/SwerveDrive.cpp
@@ -1,40 +1,40 @@
 #include "SwerveDrive.h"
+
 #include <iostream>
-//Constructor
-SwerveDrive::SwerveDrive(){}
+// Constructor
+SwerveDrive::SwerveDrive() {}
 
-
-//Drive function contains Swerve Inverse Kinematics
-void 
-SwerveDrive::Drive(double x1, double y1, double x2, double rot, bool fieldOrient){
-    double r = sqrt(m_L*m_L + m_W*m_W);
+// Drive function contains Swerve Inverse Kinematics
+void SwerveDrive::Drive(double x1, double y1, double x2, double rot,
+                        bool fieldOrient) {
+    double r = sqrt(m_L * m_L + m_W * m_W);
     y1 *= -1;
     x1 *= -1;
     x2 *= -1;
-    rot *= M_PI/180;
+    rot *= M_PI / 180;
 
-    if(fieldOrient){
-        m_temp = y1*cos(rot) + x1*sin(rot);
-        x1 = -y1*sin(rot) + x1*cos(rot);
+    if (fieldOrient) {
+        m_temp = y1 * cos(rot) + x1 * sin(rot);
+        x1 = -y1 * sin(rot) + x1 * cos(rot);
         y1 = m_temp;
     }
 
-    double a = x1 - x2 * (m_L/r);
-    double b = x1 + x2 * (m_L/r);
-    double c = y1 - x2 * (m_W/r);
-    double d = y1 + x2 * (m_W/r);
+    double a = x1 - x2 * (m_L / r);
+    double b = x1 + x2 * (m_L / r);
+    double c = y1 - x2 * (m_W / r);
+    double d = y1 + x2 * (m_W / r);
 
-    double backRightSpeed = sqrt((a*a) + (d*d));
-    double backLeftSpeed = sqrt((a*a) + (c*c));
-    double frontRightSpeed = sqrt((b*b) + (d*d));
-    double frontLeftSpeed = sqrt((b*b) + (c*c));
+    double backRightSpeed = sqrt((a * a) + (d * d));
+    double backLeftSpeed = sqrt((a * a) + (c * c));
+    double frontRightSpeed = sqrt((b * b) + (d * d));
+    double frontLeftSpeed = sqrt((b * b) + (c * c));
 
-    double backRightAngle = atan2(a, d) * 180/ M_PI;
-    double backLeftAngle = atan2(a, c) *180/ M_PI;
-    double frontRightAngle = atan2(b, d) *180/ M_PI;
-    double frontLeftAngle = atan2(b, c) *180/ M_PI;
+    double backRightAngle = atan2(a, d) * 180 / M_PI;
+    double backLeftAngle = atan2(a, c) * 180 / M_PI;
+    double frontRightAngle = atan2(b, d) * 180 / M_PI;
+    double frontLeftAngle = atan2(b, c) * 180 / M_PI;
 
-    if(x2 == 0 && x1 == 0 && y1 == 0){
+    if (x2 == 0 && x1 == 0 && y1 == 0) {
         m_backRight.Stop();
         m_backLeft.Stop();
         m_frontRight.Stop();
@@ -42,81 +42,81 @@ SwerveDrive::Drive(double x1, double y1, double x2, double rot, bool fieldOrient
         return;
     }
 
-    m_backRight.drive(-backRightSpeed, backRightAngle,
-        DriveConstants::BROFF);
-    m_backLeft.drive(backLeftSpeed, backLeftAngle,
-        DriveConstants::BLOFF);
+    m_backRight.drive(-backRightSpeed, backRightAngle, DriveConstants::BROFF);
+    m_backLeft.drive(backLeftSpeed, backLeftAngle, DriveConstants::BLOFF);
     m_frontRight.drive(-frontRightSpeed, frontRightAngle,
-        DriveConstants::FROFF);
-    m_frontLeft.drive(frontLeftSpeed, frontLeftAngle, 
-        DriveConstants::FLOFF);
+                       DriveConstants::FROFF);
+    m_frontLeft.drive(frontLeftSpeed, frontLeftAngle, DriveConstants::FLOFF);
 }
 
-
-//Updates the Odometry
-void
-SwerveDrive::UpdateOdometry(double theta){
-    double BR_A = m_backRight.getAngle(DriveConstants::BROFF);
-    double BR_V = m_backRight.getVelocity();
-    double BL_A = m_backLeft.getAngle(DriveConstants::BLOFF);
-    double BL_V = m_backLeft.getVelocity();
-    double FR_A = m_frontRight.getAngle(DriveConstants::FROFF);
-    double FR_V = m_frontRight.getVelocity();
-    double FL_A = m_frontLeft.getAngle(DriveConstants::FLOFF);
-    double FL_V = m_frontLeft.getVelocity();
-    m_odometry.updateOdometry(BR_A, BR_V, BL_A, BL_V, FR_A, FR_V,
-        FL_A, FL_V, theta);
+// Updates the Odometry
+void SwerveDrive::UpdateOdometry(double theta_rad, double dt) {
+    // NOTE I am explicitly ignoring any polarity flips,
+    // because those should be fixed elsewhere, not here.
+    // The offsets are sufficient to ensure that modules
+    // return robot-relative wheel velocities as (angle, vel)
+    // pairs.
+    double BR_A = m_backRight.getAngleRad(DriveConstants::BROFF);
+    double BR_D = m_backRight.getWheelDistance();
+    double BL_A = m_backLeft.getAngleRad(DriveConstants::BLOFF);
+    double BL_D = m_backLeft.getWheelDistance();
+    double FR_A = m_frontRight.getAngleRad(DriveConstants::FROFF);
+    double FR_D = m_frontRight.getWheelDistance();
+    double FL_A = m_frontLeft.getAngleRad(DriveConstants::FLOFF);
+    double FL_D = m_frontLeft.getWheelDistance();
+    m_odometry.updateOdometry(BR_A, BR_D, BL_A, BL_D, FR_A, FR_D, FL_A, FL_D,
+                              theta_rad, dt);
 }
 
-
-//Trajectory Following for Swerve
-//Cannot turn and move at the same time
-//If you want to turn make Waypoint as 0,0,0, theta
-void
-SwerveDrive::TrajectoryFollow(double rot, size_t waypointIndex){
+// Trajectory Following for Swerve
+// Cannot turn and move at the same time
+// If you want to turn make Waypoint as 0,0,0, theta
+void SwerveDrive::TrajectoryFollow(double rot, size_t waypointIndex) {
     size_t index = m_trajectory_1.getIndex();
-    
+
     bool YError = (abs(GetYPosition() - m_trajectory_1.getY(index)) < 0.3);
-    bool XError = (abs(GetXPostion() - m_trajectory_1.getX(index)) < 0.3);
+    bool XError = (abs(GetXPosition() - m_trajectory_1.getX(index)) < 0.3);
     bool RotError = (abs(rot - m_trajectory_1.getRotation(index)) < 5);
 
-    frc::SmartDashboard::PutNumber("RotError", (abs(rot - m_trajectory_1.getRotation(index))));
+    frc::SmartDashboard::PutNumber(
+        "RotError", (abs(rot - m_trajectory_1.getRotation(index))));
     frc::SmartDashboard::PutBoolean("ROTERROR", RotError);
 
-    if(m_trajectory_1.getX(index) == 0 &&
-        m_trajectory_1.getY(index) == 0 &&
-        m_trajectory_1.getRotation(index) != 0){
-        if(RotError && index < waypointIndex){
+    if (m_trajectory_1.getX(index) == 0 && m_trajectory_1.getY(index) == 0 &&
+        m_trajectory_1.getRotation(index) != 0) {
+        if (RotError && index < waypointIndex) {
             m_trajectory_1.Progress();
             std::cout << "Gets here once" << std::endl;
             gyro->Reset();
         }
-        Drive(0, 0, calcYawStraight(m_trajectory_1.getRotation(index), rot), rot, true);
+        Drive(0, 0, calcYawStraight(m_trajectory_1.getRotation(index), rot),
+              rot, true);
         return;
     }
 
-    m_swerveController.updatePosition(GetYPosition(), GetXPostion(), rot);
+    m_swerveController.updatePosition(GetYPosition(), GetXPosition(), rot);
 
-    //frc::SmartDashboard::PutNumber("YError", abs(GetYPosition() - m_trajectory_1.getY(index)));
-    //frc::SmartDashboard::PutNumber("XError", abs(GetXPostion() - m_trajectory_1.getX(index)));
+    // frc::SmartDashboard::PutNumber("YError", abs(GetYPosition() -
+    // m_trajectory_1.getY(index))); frc::SmartDashboard::PutNumber("XError",
+    // abs(GetXPosition() - m_trajectory_1.getX(index)));
 
-    double forward = m_swerveController.calculateForward(m_trajectory_1.getY(index));
-    double strafe = m_swerveController.calculateStrafe(m_trajectory_1.getX(index));
+    double forward =
+        m_swerveController.calculateForward(m_trajectory_1.getY(index));
+    double strafe =
+        m_swerveController.calculateStrafe(m_trajectory_1.getX(index));
 
-    if( YError && XError && RotError && index < waypointIndex){
+    if (YError && XError && RotError && index < waypointIndex) {
         m_trajectory_1.Progress();
     }
 
-    //frc::SmartDashboard::PutNumber("strafe", strafe);
+    // frc::SmartDashboard::PutNumber("strafe", strafe);
 
-    UpdateOdometry(rot);
-    Drive(strafe, forward, calcYawStraight(m_trajectory_1.getRotation(index), rot), rot, true);
+    Drive(strafe, forward,
+          calcYawStraight(m_trajectory_1.getRotation(index), rot), rot, true);
 }
 
-
-//Function for generating trajectory_1
-void
-SwerveDrive::GenerateTrajectory_1(){
+// Function for generating trajectory_1
+void SwerveDrive::GenerateTrajectory_1() {
     m_trajectory_1.clearTrajectory();
 
     // Default Scoot and Shoot
@@ -135,7 +135,7 @@ SwerveDrive::GenerateTrajectory_1(){
     Trajectory::Waypoint p5(0, 0, 0, 70);
     m_trajectory_1.addWaypoint(p1);
     m_trajectory_1.addWaypoint(p2);
-    m_trajectory_1.addWaypoint(p3);  
+    m_trajectory_1.addWaypoint(p3);
     m_trajectory_1.addWaypoint(p4);
     m_trajectory_1.addWaypoint(p5);
 
@@ -148,109 +148,67 @@ SwerveDrive::GenerateTrajectory_1(){
     // Trajectory::Waypoint p6(7.0, 1.4, 0, 0);
     // m_trajectory_1.addWaypoint(p1);
     // m_trajectory_1.addWaypoint(p2);
-    // m_trajectory_1.addWaypoint(p3);  
+    // m_trajectory_1.addWaypoint(p3);
     // m_trajectory_1.addWaypoint(p4);
     // m_trajectory_1.addWaypoint(p5);
     // m_trajectory_1.addWaypoint(p6);
 }
 
-
-void
-SwerveDrive::GenerateTrajectory_2(){
+void SwerveDrive::GenerateTrajectory_2() {
     m_trajectory_2.clearTrajectory();
     // Trajectory::Waypoint p1(0, 0, 0, 90);
 
-    // m_trajectory_2.addWaypoint(p1);  
+    // m_trajectory_2.addWaypoint(p1);
 }
 
+// Return y position of the robot
+double SwerveDrive::GetYPosition() { return m_odometry.getY(); }
 
-//Return y position of the robot
-double
-SwerveDrive::GetYPosition(){
-    return m_odometry.getY();
-}
+// Return x position of the robot
+double SwerveDrive::GetXPosition() { return m_odometry.getX(); }
 
+double SwerveDrive::GetYSpeed() { return m_odometry.getYSpeed(); }
 
-//Return x position of the robot
-double
-SwerveDrive::GetXPostion(){
-    return m_odometry.getX();
-}
+double SwerveDrive::GetXSpeed() { return m_odometry.getXSpeed(); }
 
-
-double
-SwerveDrive::GetYSpeed(){
-    return m_odometry.getYSpeed();
-}
-
-
-double
-SwerveDrive::GetXSpeed(){
-    return m_odometry.getXSpeed();
-}
-
-
-//Rotation correction - P controller
-double 
-SwerveDrive::calcYawStraight(double targetAngle, double currentAngle){
+// Rotation correction - P controller
+double SwerveDrive::calcYawStraight(double targetAngle, double currentAngle) {
     double error = targetAngle - currentAngle;
-    return 0.013*error;
+    return 0.013 * error;
 }
 
+// Function to set PID for Swerve Drive Controller
+void SwerveDrive::SetDriveControllerPID() { m_swerveController.setPID(); }
 
-//Function to set PID for Swerve Drive Controller
-void
-SwerveDrive::SetDriveControllerPID(){
-    m_swerveController.setPID();
-}
+void SwerveDrive::SetDriveControllerROTPID() { m_swerveController.setRotPID(); }
 
-
-void
-SwerveDrive::SetDriveControllerROTPID(){
-    m_swerveController.setRotPID();
-}
-
-
-//REDUNDANT!! FIX
-//Function to Set PID for Swerve Modules
-void 
-SwerveDrive::SetPID(){
+// REDUNDANT!! FIX
+// Function to Set PID for Swerve Modules
+void SwerveDrive::SetPID() {
     m_backRight.setPID();
     m_backLeft.setPID();
     m_frontRight.setPID();
     m_frontLeft.setPID();
 }
 
+// Reset Odometry
+void SwerveDrive::ResetOdometry() { m_odometry.Reset(); }
 
-//Reset Odometry
-void
-SwerveDrive::ResetOdometry(){
-    m_odometry.Reset();
-}
-
-
-//For initialization of Swerve Modules
-void
-SwerveDrive::Initialize(){
+// For initialization of Swerve Modules
+void SwerveDrive::Initialize() {
     m_backLeft.initialization(m_BL_Offset);
     m_backRight.initialization(m_BR_Offset);
     m_frontRight.initialization(m_FR_Offset);
-    m_frontLeft.initialization(m_FL_Offset);    
+    m_frontLeft.initialization(m_FL_Offset);
 }
 
-
-//Reset Swerve Module Encoders
-void
-SwerveDrive::ResetEncoders(){
+// Reset Swerve Module Encoders
+void SwerveDrive::ResetEncoders() {
     m_backLeft.resetEncoder();
     m_backRight.resetEncoder();
     m_frontLeft.resetEncoder();
     m_frontRight.resetEncoder();
 }
 
-
-//Helper Function
-void 
-SwerveDrive::debug(AHRS &navx){
-    gyro = &navx;
-}
+// Helper Function
+void SwerveDrive::debug(AHRS &navx) { gyro = &navx; }
--- src/main/include/Odometry.h
+++ src/main/include/Odometry.h
@@ -1,39 +1,32 @@
 #pragma once
 
-#include <math.h>
-#include <frc/smartdashboard/SmartDashboard.h>
 #include <Constants.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <math.h>
 
-#define PI 3.14159265
+class Odometry {
+   public:
+    Odometry();
+    void updateOdometry(double BR_A, double BR_V, double BL_A, double BL_V,
+                        double FR_A, double FR_V, double FL_A, double FL_V,
+                        double theta, double dt);
+    double getX();
+    double getY();
+    double getXSpeed();
+    double getYSpeed();
+    void Reset();
 
-class Odometry{
+   private:
+    const double timeInterval = GeneralConstants::timeStep;
+    double m_timeStep = 0;
+    double m_prevtimeStep = 0;
 
-    public:
-        Odometry();
-        void updateOdometry(double BR_A, double BR_V,
-            double BL_A, double BL_V,
-            double FR_A, double FR_V,
-            double FL_A, double FL_V,
-            double theta);
-        double getX();
-        double getY();
-        double getXSpeed();
-        double getYSpeed();
-        void Reset();
+    double m_x = 0;
+    double m_y = 0;
 
+    double m_xdot = 0;
+    double m_ydot = 0;
 
-    private:
-        const double timeInterval = GeneralConstants::timeStep;
-        double m_timeStep = 0;
-        double m_prevtimeStep = 0;
-
-        double m_x = 0;
-        double m_y = 0;
-        double m_L = DriveConstants::Length;
-        double m_W = DriveConstants::Width;
-
-        double m_FWD;
-        double m_STR;
-        double m_FWD_new;
-        double m_STR_new;
+    double init = false;
+    double prev_bl{0.0}, prev_br{0.0}, prev_fl{0.0}, prev_fr{0.0};
 };
